8e7929d2653b272603b848867e886727116cd29f
Daniel Häfele <haefele@xemio.net>
2021-06-06 18:08:04 +0200

    Work on ListCommand.cs

--- apps/CTime3.Apps.CommandLine/Commands/ListCommand.cs
+++ apps/CTime3.Apps.CommandLine/Commands/ListCommand.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using CTime3.Core;
 using CTime3.Core.Services.Configurations;
 using CTime3.Core.Services.CTime;
 using Spectre.Console;
 using Spectre.Console.Cli;
+using Color = Spectre.Console.Color;
 
 namespace CTime3.Apps.CommandLine.Commands
 {
@@ -32,15 +35,96 @@ namespace CTime3.Apps.CommandLine.Commands
                 this._ansiConsole.MarkupLine("You are not [red]logged in![/] You have to login before using this command.");
                 return ExitCodes.Failed;
             }
-
-            var times = await this._cTimeService.GetTimes(this._configurationService.Config.CurrentUser.Id, DateTime.Today.AddDays(-7), DateTime.Today);
-
-            foreach (var time in times)
-            {
-                // this._ansiConsole.MarkupLine($"{time.}");
-            }
             
+            foreach (var (start, end) in this.GetTimes())
+            {
+                IList<Time> times = null;
+                await this._ansiConsole
+                    .Status()
+                    .StartAsync("Loading times", async _ =>
+                    {
+                        times = await this._cTimeService.GetTimes(this._configurationService.Config.CurrentUser.Id, start, end);
+                    });
+
+                this.RenderTimes(times);
+            }
+
             return ExitCodes.Ok;
+        }
+
+        private IEnumerable<(DateTime start, DateTime end)> GetTimes()
+        {
+            var start = DateTime.Today.AddDays(-6);
+            var end = DateTime.Today;
+
+            while (true)
+            {
+                yield return (start, end);
+                
+                if (this._ansiConsole.Confirm("Show more?") == false)
+                    yield break;
+
+                end = start.AddDays(-1);
+                start = start.AddDays(-7);
+            }
+        }
+
+        private void RenderTimes(IList<Time> times)
+        {
+            var timesPerDay = from time in times
+                orderby time.Day descending
+                group time by time.Day
+                into g
+                select new
+                {
+                    Day = g.Key,
+                    Times = g.OrderByDescending(f => f.ClockInTime)
+                };
+
+            foreach (var perDay in timesPerDay)
+            {
+                var hasAnyTimes = perDay.Times.Any(f => f.ClockInTime != null || f.ClockOutTime != null);
+
+                var dayRule = new Rule($"[white underline]{perDay.Day:ddd dd. MMM yyyy}[/] [white dim]({(int) perDay.Times.First().Hours.TotalHours:00}:{perDay.Times.First().Hours.Minutes:00})[/]")
+                    .LeftAligned()
+                    .Border(IsWeekend(perDay.Day) ? BoxBorder.Double : BoxBorder.Square)
+                    .RuleStyle(new Style(hasAnyTimes ? Color.Green : Color.Red));
+                this._ansiConsole.Write(dayRule);
+
+                if (hasAnyTimes == false)
+                {
+                    this._ansiConsole.MarkupLine($"[dim]No times on this day.[/]");
+                    this._ansiConsole.WriteLine();
+                }
+                else
+                {
+                    var titleLength = Markup.Remove(dayRule.Title).Length;
+                    var durationLength = "(00:00)".Length;
+                    var rulePaddingLeft = 3;
+
+                    foreach (var time in perDay.Times)
+                    {
+                        var from = time.ClockInTime?.ToString("t") ?? "?";
+                        var to = time.ClockOutTime?.ToString("t") ?? "?";
+                        var duration = time.ClockInTime != null && time.ClockOutTime != null
+                            ? time.ClockOutTime.Value - time.ClockInTime.Value
+                            : (TimeSpan?) null;
+
+                        var timeAsString = $"{@from} - {to}".PadRight(titleLength + rulePaddingLeft - durationLength);
+                        if (duration != null)
+                            timeAsString += $"[dim]({(int) duration.Value.TotalHours:00}:{duration.Value.Minutes:00})[/]";
+
+                        this._ansiConsole.MarkupLine(timeAsString);
+                    }
+
+                    this._ansiConsole.WriteLine();
+                }
+            }
+        }
+
+        private bool IsWeekend(DateTime date)
+        {
+            return date.DayOfWeek is DayOfWeek.Saturday or DayOfWeek.Sunday;
         }
     }
 }
