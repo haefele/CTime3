e49570765287710ba5ceaf1f0eea9861002f7030
Daniel Häfele <haefele@xemio.net>
2023-03-01 00:12:23 +0100

    Add a CTime3.Scripts project

---
+++ deployment/CTime3.Scripts/CTimePaths.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.ObjectModel;
+
+namespace CTime3.Scripts;
+
+public static class CTimePaths
+{
+    public static string SolutionDirectory
+    {
+        get
+        {
+            var currentFolder = new DirectoryInfo(Path.GetDirectoryName(typeof(CTimePaths).Assembly.Location)!);
+            return currentFolder.Parent?.Parent?.Parent?.Parent?.Parent?.FullName ?? throw new InvalidOperationException("Could not determine the SolutionDirectory");
+        }
+    }
+
+    public static string ArtifactsDirectory => Path.Combine(SolutionDirectory, "artifacts");
+
+    public static ReadOnlyCollection<string> ProjectDirectories => new[]
+        {
+            CTime3Core.ProjectDirectory,
+            CTime3AppAvalonia.ProjectDirectory,
+            CTime3AppCommandLine.ProjectDirectory,
+        }.ToList().AsReadOnly();
+
+    public static ReadOnlyCollection<string> BinAndObjDirectories => new[]
+        {
+            ProjectDirectories.Select(f => Path.Combine(f, "bin")),
+            ProjectDirectories.Select(f => Path.Combine(f, "obj")),
+        }.SelectMany(f => f).ToList().AsReadOnly();
+
+    public static class CTime3Core
+    {
+        public static string ProjectDirectory => Path.Combine(SolutionDirectory, "src", "CTime3.Core");
+    }
+
+    public static class CTime3AppAvalonia
+    {
+        public static string ProjectDirectory => Path.Combine(SolutionDirectory, "apps", "CTime3.Apps.Avalonia");
+    }
+
+    public static class CTime3AppCommandLine
+    {
+        public static string Runtime => "win-x64";
+        public static string TargetFramework => "net7.0";
+
+        public static string ProjectDirectory => Path.Combine(SolutionDirectory, "apps", "CTime3.Apps.CommandLine");
+        public static string PublishDirectory => Path.Combine(ProjectDirectory, "bin", "Release", TargetFramework, Runtime, "publish");
+        public static string ArtifactsFile => Path.Combine(ArtifactsDirectory, "CTime3.Apps.CommandLine.zip");
+    }
+}
---
+++ deployment/CTime3.Scripts/Program.cs
@@ -0,0 +1,43 @@
+﻿using CliWrap;
+using static Bullseye.Targets;
+using static CTime3.Scripts.FileHelper;
+using static CTime3.Scripts.CTimePaths;
+
+Target("clean", () =>
+{
+    DeleteDirectory(ArtifactsDirectory);
+    Console.WriteLine("Cleaned artifacts directory");
+
+    foreach (var directory in BinAndObjDirectories)
+    {
+        DeleteDirectory(directory);
+    }
+    Console.WriteLine("Cleaned bin and obj directories");
+});
+
+Target("build-cli", DependsOn("clean"), () =>
+{
+    Cli.Wrap("dotnet")
+        .WithArguments(f => f
+            .Add("publish")
+            .Add(CTime3AppCommandLine.ProjectDirectory)
+            .Add("-r").Add(CTime3AppCommandLine.Runtime)
+            .Add("--self-contained")
+            .Add("-c").Add("Release")
+            .Add("-f").Add(CTime3AppCommandLine.TargetFramework)
+            .Add("/p:PublishSingleFile=true,IncludeNativeLibrariesForSelfExtract=true"))
+        .ExecuteAsync().GetAwaiter().GetResult();
+    Console.WriteLine("Built CTime3.Apps.CommandLine");
+
+    RenameFile(CTime3AppCommandLine.PublishDirectory, "CTime3.Apps.CommandLine.exe", "ctime.exe");
+    Console.WriteLine("Renamed files");
+
+    ZipDirectory(CTime3AppCommandLine.PublishDirectory, CTime3AppCommandLine.ArtifactsFile);
+    Console.WriteLine("Zipped artifact");
+});
+
+Target("build", DependsOn("build-cli"));
+
+Target("default", DependsOn("build"));
+
+RunTargetsAndExit(args);
