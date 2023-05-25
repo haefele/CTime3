9ca51a241e6f11aec982642e29bf26d4511c964f
Daniel Häfele <haefele@xemio.net>
2023-05-26 00:05:38 +0200

    Add better error handling

---
+++ src/CTime3.Core/Extensions/ExceptionExtensions.cs
@@ -0,0 +1,60 @@
+﻿using System.Reflection;
+using System.Text;
+
+namespace CTime3.Core.Extensions;
+
+public static class ExceptionExtensions
+{
+    public static string GetFullMessage(this Exception self)
+    {
+        Guard.IsNotNull(self);
+
+        var sb = new StringBuilder();
+        foreach (var ex in self.GetUserRelevantExceptions())
+        {
+            sb.AppendLine(ex.Message);
+        }
+
+        return sb.ToString();
+    }
+
+    public static IEnumerable<Exception> GetUserRelevantExceptions(this Exception self)
+    {
+        Guard.IsNotNull(self);
+
+        return self.GetAllExceptions()
+            .Where(f => f is not AggregateException)
+            .Where(f => f is not TargetInvocationException);
+    }
+
+    public static IEnumerable<Exception> GetAllExceptions(this Exception self)
+    {
+        Guard.IsNotNull(self);
+
+        var stack = new Stack<Exception>();
+        stack.Push(self);
+
+        while (stack.TryPop(out var current))
+        {
+            yield return current;
+
+            switch (current)
+            {
+                case AggregateException { InnerExceptions: not null } aggEx:
+                    {
+                        foreach (var inner in aggEx.InnerExceptions)
+                        {
+                            stack.Push(inner);
+                        }
+
+                        break;
+                    }
+                case { InnerException: not null }:
+                    {
+                        stack.Push(current.InnerException);
+                    }
+                    break;
+            }
+        }
+    }
+}
